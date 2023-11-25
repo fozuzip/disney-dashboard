beb97021606fa9b15f4efa5f2c0c47ceeaff9753
George Pittas <fozuzip@gmail.com>
2023-11-25 13:34:00 +0200

    Add Redux store and hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { useAppSelector, useAppDispatch } from "@/hooks";
+
+import { increment, decrement } from "@/counterSlice";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
+  return (
+    <>
+      <div>
+        <button onClick={handleDecrement}>-</button>
+        <span>{count}</span>
+        <button onClick={handleIncrement}>+</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
